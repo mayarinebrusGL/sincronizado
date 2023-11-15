4894cfddb9fb51885c82712af3f52ba07d86525e
mayarinebrusGL <mayari.nebrus@gorillalogic.com>
2023-11-15 16:04:17 -0600

    primerComit

---
+++ initials.java
@@ -0,0 +1,11 @@
+class initials{
+    public static void main(String[] args) {
+        System.out.println("MM     MM N       N");
+        System.out.println("M M   M M N N     N");
+        System.out.println("M   M   M N   N   N");
+        System.out.println("M       M N     N N");
+        System.out.println("M       M N       N");
+        System.out.println("M       M N       N");
+        System.out.println("M       M N       N");
+    }
+}
